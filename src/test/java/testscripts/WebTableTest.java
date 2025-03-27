e4057ba71e25c216d2030f265e37facfc6744270
1-Kavya <kavya.rarl@gmail.com>
2025-03-27 10:22:32 +0530

    Day2 code

--- src/test/java/testscripts/WebTableTest.java
+++ src/test/java/testscripts/WebTableTest.java
@@ -1,6 +1,10 @@
 package testscripts;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -9,8 +13,20 @@ public class WebTableTest {
   public void dynamicWebTable() {
 	  
 	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
 	  driver.get("https://testautomationpractice.blogspot.com/");
-	  List<webelements> items = driver.findelements();
+	  List<WebElement> items = driver.findElements(
+			  By.xpath("//td[contains(text(), 'Master')]//following-sibling::td"));
+//	  System.out.println("Details......"+items.size());
+//	  for(WebElement item:items) {
+//		  System.out.println(item.getText());
+//	  }
 	  
+	  String authorName = driver.findElement(
+			  By.xpath("//td[contains(text(), '3000')]//preceding-sibling::td[3]")).getText();
+	  System.out.println("Details......"+items.size());
+	  for(WebElement item:items) {
+		  System.out.println(item.getText());
+	  }
   }
 }
